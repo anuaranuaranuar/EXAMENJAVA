6592ae8a2be2de4c76a6cdeb1f70dc164b4300af
anuaranuaranuar <51975464+anuaranuaranuar@users.noreply.github.com>
2019-07-02 18:39:03 -0500

    ingresando datos

---
+++ EXAMENJAVA/src/examenjava/EXAMENJAVA.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenjava;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ANUAR
+ */
+public class EXAMENJAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner l = new Scanner(System.in);
+        ArrayList<String> nomb = new ArrayList<String>();
+        ArrayList<Float> t = new ArrayList<Float>();
+        ArrayList<Float> v = new ArrayList<Float>();
+        int op = 1;
+        float vac=0;
+        
+       
+        
+        while (op != 0) {           
+            System.out.println("ingrese nombre:");
+            nomb.add(l.next());
+            System.out.println("ingrese la velocidad y si desea salir digite cero:");
+            op = l.nextInt();
+            if(op != 0){
+                v.add((float) op);
+                t.add(100 / v.get(v.size()-1));
+                vac =vac + t.get(t.size()-1);
+            }}
+        
+        
+    }
+    
+}
+
+    
+    
+
